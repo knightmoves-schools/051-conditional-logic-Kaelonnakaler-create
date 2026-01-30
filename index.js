8bad1cf4974c760f6a41ec72897cfd7f294fd695
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-01-30 13:27:26 -0600

    Fix validateAge function syntax and logic

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function validateAge(oldEnough){
   // add your code here
 }
-let validateAge = oldEnough;
+function validateAge = (oldEnough);
 return oldEnough = true;
-let validateAge = oldEnough = false;
+ 
 
-document.getElementById("result").innerHTML = 'ran it';}
+document.getElementById("result").innerHTML = 'ran it';
+

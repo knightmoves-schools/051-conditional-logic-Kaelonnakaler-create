116c82abc09226649520310fe64efdf9ef82316e
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-02 09:37:32 -0600

    added {
    Refactor validateAge function and fix return statement.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function validateAge(oldEnough){
   // add your code here
 }
-function validateAge = (oldEnough);
-return oldEnough = true;
- 
+function validateAge = (oldEnough){;
+return 'old Enough' = false
+                                  };
 
 document.getElementById("result").innerHTML = 'ran it';
+
 

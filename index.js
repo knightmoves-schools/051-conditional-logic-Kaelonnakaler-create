25d01e496e3684b3f853bbe282466d12b98f6030
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-03 14:04:06 -0600

    Fix validateAge function logic and formatting

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
  function validateAge(oldEnough){
   // add your code here
 }
-logic function validateAge(oldEnough){ = true; 
-return "old Enough" = false
+logic function validateAge(oldEnough){ = false; 
+return "old Enough" = true;
                                   };
 
 document.getElementById("result").innerHTML = 'ran it';
+
 
 
 
